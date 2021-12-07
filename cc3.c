a1499402f876610434ae52346e33a4824ebb0b98
charlyli17 <kilarichristine@gmail.com>
2021-12-07 18:13:02 +0100

    commit

--- cc3.c
+++ cc3.c
@@ -1,109 +1,133 @@
 typedef int TElement;
 
-typedef struct noeud{
+typedef struct noeud
+{
     TElement donnee;
-    struct noeud *fg,*fd;
-}*Arbre;
+    struct noeud *fg, *fd;
+} * Arbre;
 
-int occElt(TElement elt, Arbre a){
-    int cpt=0;
-    if(a==NULL){
+int occElt(TElement elt, Arbre a)
+{
+    int cpt = 0;
+    if (a == NULL)
+    {
         return 0;
     }
-    if(a->donnee==elt){
-        return 1+occElt(elt,a->fg)+occElt(elt,a->fd);
+    if (a->donnee == elt)
+    {
+        return 1 + occElt(elt, a->fg) + occElt(elt, a->fd);
     }
-    
 }
 
-int estEquilibre(Arbre a){
-    if(a==NULL){
+int estEquilibre(Arbre a)
+{
+    if (a == NULL)
+    {
         return 0;
     }
-    int cpp=0;
-    int cpi=0;
+    int cpp = 0;
+    int cpi = 0;
 
-    if((a->donnee)%2==0){
+    if ((a->donnee) % 2 == 0)
+    {
         cpp++;
-    }else cpi++;
+    }
+    else
+        cpi++;
 
     occElt(a->fg);
     occElt(a->fd);
 
-    return (cpp==cpi) ? 1 : 0;
+    return (cpp == cpi) ? 1 : 0;
 }
 
-int estEquilibre (Arbre a){
-    if(a==NULL){
+int estEquilibre(Arbre a)
+{
+    if (a == NULL)
+    {
         return 0;
     }
-    
-    int cpp=0;
-    int cpi=0;
 
-    if((a->donnee)%2==0){
-        cpp+=1+estEquilibre(a->fg)+estEquilibre(a->fd);
-    }else{
-        cpi+=1+estEquilibre(a->fg)+estEquilibre(a->fd);
+    int cpp = 0;
+    int cpi = 0;
+
+    if ((a->donnee) % 2 == 0)
+    {
+        cpp += 1 + estEquilibre(a->fg) + estEquilibre(a->fd);
+    }
+    else
+    {
+        cpi += 1 + estEquilibre(a->fg) + estEquilibre(a->fd);
     }
 
-    return (cpp==cpi) ? 1:0;
+    return (cpp == cpi) ? 1 : 0;
 }
 
-Arbre creatFeuille(TElement elt){
-    Arbre a= NULL;
-    a=(Arbre)malloc(sizeof(struct noeud *));
-    a->donnee=elt;
-    a->fg=NULL;
-    a->fd=NULL;
+Arbre creatFeuille(TElement elt)
+{
+    Arbre a = NULL;
+    a = (Arbre)malloc(sizeof(struct noeud *));
+    a->donnee = elt;
+    a->fg = NULL;
+    a->fd = NULL;
     return a;
 }
 
-Arbre estVideA(Arbre a){
-    if(a==NULL){
+Arbre estVideA(Arbre a)
+{
+    if (a == NULL)
+    {
         return 1;
     }
     return 0;
 }
 
-Arbre creatNoeud(TElement elt, Arbre fg, Arbre fd){
-    a=creatFeuille(elt);
-    a->fg=fg;
-    a->fd=fd;
+Arbre creatNoeud(TElement elt, Arbre fg, Arbre fd)
+{
+    a = creatFeuille(elt);
+    a->fg = fg;
+    a->fd = fd;
     return a;
 }
 
-typedef struct cellule{
+typedef struct cellule
+{
     TElement donnee;
     struct cellule *suivant;
-}*Pile;
+} * Pile;
 
-Pile initP(){
+Pile initP()
+{
     return NULL;
 }
 
-Pile empiler(TElement elt, Pile p){
-    Pile newp=NULL;
-    newp=(Pile)malloc(sizeof(struct cellule));
-    newp->donnee=elt;
+Pile empiler(Arbre a, Pile p)
+{
+    Pile newp = NULL;
+    newp = (Pile)malloc(sizeof(struct cellule));
+    newp->donnee = a->donnee;
+    newp->suivant = p;
     return newp;
 }
 
-Arbre parcoursLargeur(Arbre a){
-    if(estVideA(a)){
+Arbre parcoursLargeur(Arbre a)
+{
+    if (estVideA(a))
+    {
         return NULL;
     }
-    Pile p=initP();
-    
+    Pile p = initP();
+    p = empiler(a, p);
 }
 
-void main(){
-    Arbre a=NULL;
-    Arbre f5=creatFeuille(5);
-    Arbre f10=creatFeuille(10);
-    Arbre f9=creatNoeud(9,f5,f10);
-    a=creatNoeud(9,NULL,f9);
+void main()
+{
+    Arbre a = NULL;
+    Arbre f5 = creatFeuille(5);
+    Arbre f10 = creatFeuille(10);
+    Arbre f9 = creatNoeud(9, f5, f10);
+    a = creatNoeud(9, NULL, f9);
 
-    int occ=occElt(a);
-    printf("Nombre d'occurence de la valeur dans l'arbre: %d",occ);
+    int occ = occElt(a);
+    printf("Nombre d'occurence de la valeur dans l'arbre: %d", occ);
 }
